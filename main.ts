0a68089517ce59a30c68e59c171b220ec4613772
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-09 13:38:12 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(0)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showNumber(2)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showNumber(1)
+})
 basic.forever(function () {
 	
 })
